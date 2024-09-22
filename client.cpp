038b17d167b40e9173ef5aaae910a6fbd7908e95
rodrigoorozco <rodrigoorozco025@gmail.com>
2024-09-22 08:44:00 +0000

    requesting data points, hard coded for 1000 req

--- client.cpp
+++ client.cpp
@@ -12,16 +12,17 @@
 */
 #include "common.h"
 #include "FIFORequestChannel.h"
-#include <iostream>
+#include <sstream>
 
 using namespace std;
 
 
 int main (int argc, char *argv[]) {
 	int opt;
-	int p = 1;
-	double t = 0.0;
-	int e = 1;
+	// -1 for data request flag
+	int p = -1;
+	double t = -1.0;
+	int e = -1;
 	string filename = "";
 
 	//Add other arguments here
@@ -47,28 +48,81 @@ int main (int argc, char *argv[]) {
 	pid_t child = fork();
 	if (child == 0)
 	{
-		char* cmd[] = {(char*)"./server", (char*)"NULL"};
-		execvp(cmd[0], cmd);		
+		char* cmd[] = {(char*)"./server", (char*)"NULL"};		// arguments for server
+		execvp(cmd[0], cmd);									// run server
 	}
 
 	FIFORequestChannel chan("control", FIFORequestChannel::CLIENT_SIDE);
-
-
 
 	//Task 4:
 	//Request a new channel
 	
 	//Task 2:
 	//Request data points
-    char buf[MAX_MESSAGE];
-    datamsg x(1, 0.0, 1);
+
+	if (p != -1 && t != -1.0 && e != -1)
+	{	// single data point
+		char buf[MAX_MESSAGE];
+		// datamsg x(1, 0.0, 1);
+		datamsg x(p, t, e);
+		memcpy(buf, &x, sizeof(datamsg));
+		chan.cwrite(buf, sizeof(datamsg));
+		double reply;
+		chan.cread(&reply, sizeof(double));
+		cout << "For person " << p << ", at time " << t << ", the value of ecg " << e << " is " << reply << endl;
+	}
+	else if (p != -1)
+	{
+		ofstream received("received/x1.csv");
+		string path = "BIMDC/9.csv";
+		ifstream bimdc(path);
+		string line;
+		int count = 1;
+
+		while (getline(bimdc, line))
+		{
+			if (count > 1000){
+				break;
+			}
+
+			string item;
+			vector<string> parsed_line;
+			std::stringstream ss(line);
+
+			while (getline(ss, item, ','))
+			{
+				parsed_line.push_back(item);
+			}
+			t = stof(parsed_line[0]);
+			e = 1;
+
 	
-	memcpy(buf, &x, sizeof(datamsg));
-	chan.cwrite(buf, sizeof(datamsg));
-	double reply;
-	chan.cread(&reply, sizeof(double));
-	cout << "For person " << p << ", at time " << t << ", the value of ecg " << e << " is " << reply << endl;
-	
+			char buf[MAX_MESSAGE];
+			datamsg x(p, t, e);
+			memcpy(buf, &x, sizeof(datamsg));
+			chan.cwrite(buf, sizeof(datamsg));
+			double reply;
+			chan.cread(&reply, sizeof(double));
+			received << t << ", " << reply;
+			received.flush();
+
+			e = 2;
+			char buf2[MAX_MESSAGE];
+			datamsg y(p, t, e);
+			memcpy(buf2, &y, sizeof(datamsg));
+			chan.cwrite(buf2, sizeof(datamsg));
+			double reply2;
+			chan.cread(&reply2, sizeof(double));
+			received << ", " << reply2 << endl;
+			received.flush();
+
+			count++;
+		}
+		bimdc.close();
+		received.close();
+	}
+
+
 	//Task 3:
 	//Request files
 	filemsg fm(0, 0);
